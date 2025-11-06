5aeb78f3b267a3f0b9252e67ee7d1cf91bdfec04
KrisChe8 <krischernobai@gmail.com>
2025-11-06 15:28:12 +0200

    add tests for hover elements and dynamic controls

--- cypress/e2e/day3.cy.js
+++ cypress/e2e/day3.cy.js
@@ -31,3 +31,55 @@ describe("Advanced Selectors", () => {
     cy.contains("a", "Logout").click();
   });
 });
+describe("Hovers checks", () => {
+  it("Verify that the user's name appears when you hover over their avatar", () => {
+    cy.visit("https://the-internet.herokuapp.com/");
+    cy.get('a[href="/hovers"]').click();
+    cy.contains("h3", "Hovers");
+    // Отримуємо всі блоки .figure
+    cy.get(".figure").each(($el, index) => {
+      cy.wrap($el).find(".figcaption").should("not.be.visible");
+      //   cy.wrap($el).realHover();
+      //   // Додаємо невелику затримку, щоб спрацював CSS-перехід для This element <div.figcaption> is not visible because it has CSS property: display: none
+      //   cy.wait(300);
+      //   cy.wrap($el)
+      //     .find(".figcaption")
+      //     .should("be.visible")
+      //     .and("contain.text", "name:");
+
+      cy.wrap($el)
+        .find(".figcaption h5")
+        .invoke("text")
+        .should("contain", "name:");
+      //   перевірити “зникнення” після відведення миші
+      cy.get("body").realHover();
+      cy.wrap($el).find(".figcaption").should("not.be.visible");
+    });
+  });
+});
+describe("Dynamic Controls", () => {
+  beforeEach(() => {
+    cy.visit("https://the-internet.herokuapp.com/");
+    cy.get('a[href="/dynamic_controls"]').click();
+  });
+  it("remove and add the checkbox", () => {
+    // cy.visit("https://the-internet.herokuapp.com/");
+    // cy.get('a[href="/dynamic_controls"]').click();
+    cy.contains("h4", "Dynamic Controls");
+    cy.get("#checkbox").should("be.visible");
+    cy.contains("button", "Remove").click();
+    cy.get("#checkbox").should("not.exist");
+    cy.contains("button", "Remove").should("not.exist");
+    cy.contains("button", "Add").click();
+    cy.get("#checkbox").should("be.visible");
+  });
+  it.only("Enter text in the disabled input field after clicking the Enable button", () => {
+    cy.contains("h4", "Dynamic Controls");
+    cy.get("#input-example input").should("be.disabled");
+    cy.contains("button", "Enable").click();
+    cy.get("#input-example input")
+      .should("be.enabled")
+      .type("Hello Cypress!")
+      .should("have.value", "Hello Cypress!");
+  });
+});
